7241ac0e325ace7a5ebee37f70c0fad2dd272423
SavaniHasadara <91213642+SavaniHasadara@users.noreply.github.com>
2023-07-30 16:44:48 +0530

    StationMaster

--- web_frontend/src/pages/controlCenter/AddCheckerClerk.jsx
+++ web_frontend/src/pages/controlCenter/AddCheckerClerk.jsx
@@ -1,39 +1,46 @@
 import React , { useState } from "react";
-import './src/css/AddCheckerClerk.css';
+/*import Logo from '../common/logoText.png';*/
+import './AddCheckerClerk.css';
 
 const AddCheckerClerk = () => {
     return (
         <div>
-            <h1>Add Ticket Checkers/Clerks</h1>
+            <h1>Add Ticket Checkers/ Ticket Clerks</h1>
         
-
-        <form>
-        <label>Add photo:
-        <input type="image" src=" " alt="Submit" width="48" height="48"/>
-        </label>
-        <label>Full Name:
-        <input type="text"/>
-        </label>
-        <label>Job Position:
-        <input type="text"/>
-        </label>
-        <label>Date of Birth:
-        <input type="date"/>
-        </label>
-        <label>Username:
-        <input type="text"/>
-        </label>
-        <label>Email Address:
-        <input type="email"/>
-        </label>
-        <label>Password:
-        <input type="password"/>
-        </label>
-        <label>Confirm Password:
-        <input type="password"/>
-        </label>
-        </form>
-
+            <div className="form_container">
+                <form className="form">
+                <label>Add photo:
+                <input type="image" src=" " alt="" width="48" height="48"/>
+                </label>
+               
+                <label>Full Name:
+                <input type="text"/>
+                </label>
+                <label>Job Position:
+                <select>
+                <option value="">Ticket Clerk</option>
+                <option value="">Ticket Checker</option>
+                </select>
+                </label>
+                <label>Date of Birth:
+                <input type="date"/>
+                </label>
+                
+                <label>Username:
+                <input type="text"/>
+                </label>
+                <label>Email Address:
+                <input type="email"/>
+                </label>
+                <label>Password:
+                <input type="password"/>
+                </label>
+                <label>Confirm Password:
+                <input type="password"/>
+                </label>
+                <button>Add</button>
+                </form>
+            </div>
         </div>
         
     );
