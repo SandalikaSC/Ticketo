87e2d9075462ad53c26df1428132e5185f809275
nadee-darshika <88460737+nadee-darshika@users.noreply.github.com>
2023-08-07 13:42:19 +0530

    completed train information page

--- web_frontend/src/pages/controlCenter/TrainInfo.jsx
+++ web_frontend/src/pages/controlCenter/TrainInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 // import IconButton from "@mui/material/IconButton";
@@ -10,6 +10,9 @@ import Grid from "@mui/material/Grid";
 // import Checkbox from "@mui/material/Checkbox";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { FormControl, InputLabel, Select } from "@mui/material";
+import ccBackgroundImage from "../../assets/cc_bg2.png";
+import StationCard from "../../components/controlCenter/StationCard";
+import "../../css/stationCard.css";
 
 const theme = createTheme({
   palette: {
@@ -26,6 +29,7 @@ const theme = createTheme({
 });
 
 const TrainInfo = () => {
+  const [selectedSchedule, setSelectedSchedule] = useState(null);
   // const [numberOfCoaches, setNumberOfCoaches] = useState(1);
 
   const scheduleData = [
@@ -39,6 +43,10 @@ const TrainInfo = () => {
     { trainName: "Train B", departure: "11:30 AM", arrival: "1:30 PM" },
     // Add more schedule items here
   ];
+
+  const handleScheduleClick = (index) => {
+    setSelectedSchedule(scheduleData[index]);
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -196,6 +204,7 @@ const TrainInfo = () => {
                 {scheduleData.map((item, index) => (
                   <div
                     key={index}
+                    onClick={() => handleScheduleClick(index)}
                     style={{
                       padding: "10px",
                       marginBottom: "10px",
@@ -232,6 +241,179 @@ const TrainInfo = () => {
               </div>
             </div>
           </Grid>
+          {/* <div
+            style={{
+              flex: 1,
+              width: "90%",
+              marginTop: "3%",
+              marginBottom: "3%",
+              marginLeft: "26px",
+              borderRadius: "20px",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <img
+              src={ccBackgroundImage}
+              alt="Illustration"
+              style={{
+                width: "80%", // Set the width of the image
+                height: "100%", // Let the height adjust proportionally
+                marginTop: "20px", // Adjust the spacing as needed
+                marginLeft: "25%",
+              }}
+            />
+          </div> */}
+          <div
+            style={{
+              flex: 1,
+              width: "90%",
+              marginTop: "2%",
+              marginBottom: "3%",
+              marginLeft: "18%",
+              borderRadius: "20px",
+              textAlign: "center",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            {selectedSchedule ? (
+              <div
+                style={{
+                  backgroundColor: "white",
+                  borderRadius: "20px",
+                  padding: "4% ",
+                  height: "94%",
+                }}
+              >
+                <h2>Galu Kumari</h2>
+                <hr></hr>
+                <div
+                  style={{
+                    display: "flex",
+                    marginTop: "3%",
+                    gap: "40%",
+                    textAlign: "center",
+                    paddingLeft: "13%",
+                  }}
+                >
+                  <h3>Start At</h3>
+                  <h3>End At</h3>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    marginTop: "3%",
+                    gap: "40%",
+                    textAlign: "center",
+                    paddingLeft: "12%",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      textAlign: "center",
+                    }}
+                  >
+                    <h3 style={{ color: "#3D51A9" }}>Galle</h3>
+                    <h4 style={{ color: "#FA6F5D" }}>8.00 a.m</h4>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      textAlign: "center",
+                    }}
+                  >
+                    <h3 style={{ color: "#3D51A9" }}>Colombo</h3>
+                    <h4 style={{ color: "#FA6F5D" }}>12.00 a.m</h4>
+                  </div>
+                </div>
+                <h3
+                  style={{
+                    textAlign: "left",
+                    marginTop: "4%",
+                    marginBottom: "3%",
+                    color: "#3D51A9",
+                  }}
+                >
+                  Stop Stations
+                </h3>
+                {/* <div></div> */}
+                <div class="schedule-topic">
+                  <h5>Station Name</h5>
+                  <h5 style={{ marginLeft: "22%" }}>Arrival</h5>
+                  <h5 style={{ marginRight: "5%" }}>Departure</h5>
+                </div>
+                <div style={{ height: "350px", overflow: "auto" }}>
+                  <StationCard
+                    stationName="Station Name 1"
+                    arrivalTime="09:00 AM"
+                    departureTime="09:15 AM"
+                  />
+                  <StationCard
+                    stationName="Station Name 1"
+                    arrivalTime="09:00 AM"
+                    departureTime="09:15 AM"
+                  />
+                  <StationCard
+                    stationIcon="path/to/your/icon.png" // Update with your icon path
+                    stationName="Station Name 1"
+                    arrivalTime="09:00 AM"
+                    departureTime="09:15 AM"
+                  />
+                  <StationCard
+                    stationIcon="path/to/your/icon.png" // Update with your icon path
+                    stationName="Station Name 1"
+                    arrivalTime="09:00 AM"
+                    departureTime="09:15 AM"
+                  />
+                  <StationCard
+                    stationIcon="path/to/your/icon.png" // Update with your icon path
+                    stationName="Station Name 1"
+                    arrivalTime="09:00 AM"
+                    departureTime="09:15 AM"
+                  />
+
+                  <StationCard
+                    stationIcon="path/to/your/icon.png" // Update with your icon path
+                    stationName="Station Name 2"
+                    arrivalTime="10:30 AM"
+                    departureTime="10:45 AM"
+                  />
+                  <StationCard
+                    stationIcon="path/to/your/icon.png" // Update with your icon path
+                    stationName="Station Name 2"
+                    arrivalTime="10:30 AM"
+                    departureTime="10:45 AM"
+                  />
+                  <StationCard
+                    stationIcon="path/to/your/icon.png" // Update with your icon path
+                    stationName="Station Name 2"
+                    arrivalTime="10:30 AM"
+                    departureTime="10:45 AM"
+                  />
+                  <StationCard
+                    stationIcon="path/to/your/icon.png" // Update with your icon path
+                    stationName="Station Name 2"
+                    arrivalTime="10:30 AM"
+                    departureTime="10:45 AM"
+                  />
+
+                  {/* Add more StationCard components as needed */}
+                </div>
+              </div>
+            ) : (
+              <img
+                src={ccBackgroundImage}
+                alt="Illustration"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  marginTop: "2%",
+                }}
+              />
+            )}
+          </div>
         </Grid>
       </div>
     </ThemeProvider>
