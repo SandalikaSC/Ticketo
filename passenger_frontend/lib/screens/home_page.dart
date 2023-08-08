630515a8fe8322a1539774aed49c87ada7dae964
SandalikaSC <66319075+SandalikaSC@users.noreply.github.com>
2023-08-08 20:08:59 +0530

    home page

--- passenger_frontend/lib/screens/home_page.dart
+++ passenger_frontend/lib/screens/home_page.dart
@@ -9,15 +9,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Container(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
-                  color: Styles.primaryColor,
+                  decoration: const BoxDecoration(
+                      color: Styles.primaryColor,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(30),
+                          bottomRight: Radius.circular(30))),
+                  width: double.infinity,
+                  height: 400,
                   child: Column(
                     children: [
                       const Gap(85),
@@ -36,155 +42,194 @@ class HomePage extends StatelessWidget {
                               ),
                             ),
                           ),
-                          const Gap(20),
+                          const Gap(10),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                "Good Morning! 👋",
+                                "${getGreeting()}!👋",
                                 style: Styles.headWhite,
                               ),
                               const Gap(5),
                               Text(
-                                "John Doe",
+                                "Sandalika Chamari",
                                 style: Styles.headWhite,
                               )
                             ],
                           ),
-                          Container(
-                            width: 150, // Adjust the width as needed
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 16),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // Add your onPressed function here
-                                  // Navigating to LoginPage when "Login" button is pressed
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => LoginPage(),
-                                    ),
-                                  );
-                                },
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          Colors.transparent),
-                                  // No color background
-                                  shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                      // Rounded corner border
-                                      side: const BorderSide(
-                                          color: Colors
-                                              .white), // White border color
-                                    ),
-                                  ),
-                                ),
-                                child: const Text(
-                                  'Login',
-                                  style: TextStyle(
-                                    color: Styles.textColor1,
-                                    fontStyle: FontStyle.normal,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          )
                         ],
                       ),
                     ],
                   ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Styles.backgroundColor2,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 400, // Set the height as needed
-            child: Align(
-              alignment: Alignment.topCenter,
+              ],
+            ),
+            Center(
               child: FractionalTranslation(
                 translation: const Offset(0, 0.45),
                 child: Container(
                   width: 350,
-                  // Set the width of the child container
                   height: 400,
-                  // Set the height of the child container
                   decoration: BoxDecoration(
                     color: Styles.backgroundColor,
-                    // Customize the color and opacity of the layer
-                    borderRadius: BorderRadius.circular(
-                        20), // Add rounded corner border if needed
+                    borderRadius: BorderRadius.circular(30),
                   ),
-                  padding: const EdgeInsets.all(16.0),
-                  child: const Column(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(Icons.location_on_outlined),
-                          Gap(10),
-                          Expanded(
-                            child: TextField(
-                              decoration:
-                                  InputDecoration(labelText: 'Start Station'),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(Icons.location_on),
-                          Gap(10),
-                          Expanded(
-                            child: TextField(
-                              decoration:
-                                  InputDecoration(labelText: 'End Station'),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(Icons.edit_calendar_outlined),
-                          Gap(10),
-                          Expanded(
-                            child: TextField(
-                              decoration:
-                                  InputDecoration(labelText: 'Journey Date'),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          TextField(
-                            decoration:
-                                InputDecoration(labelText: 'Start Station'),
-                          ),
-                          TextField(
-                            decoration:
-                                InputDecoration(labelText: 'Start Station'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                  child: const TrainScheduleForm(),
                 ),
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TrainScheduleForm extends StatefulWidget {
+  const TrainScheduleForm({super.key});
+
+  @override
+  _TrainScheduleFormState createState() => _TrainScheduleFormState();
+}
+
+class _TrainScheduleFormState extends State<TrainScheduleForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? _startStation;
+  String? _endStation;
+  TimeOfDay? _startTime;
+  TimeOfDay? _endTime;
+  DateTime? _selectedDate;
+  String? _selectedTicketClass;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Start Station'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a start station';
+              }
+              return null;
+            },
+            onChanged: (value) {
+              // Handle station filtering based on user input
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'End Station'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter an end station';
+              }
+              return null;
+            },
+            onChanged: (value) {
+              // Handle station filtering based on user input
+            },
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final time = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.now(),
+                    );
+                    if (time != null) {
+                      setState(() {
+                        _startTime = time;
+                      });
+                    }
+                  },
+                  child: Text(_startTime == null
+                      ? 'Select Start Time'
+                      : 'Start Time: ${_startTime!.format(context)}'),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final time = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.now(),
+                    );
+                    if (time != null) {
+                      setState(() {
+                        _endTime = time;
+                      });
+                    }
+                  },
+                  child: Text(_endTime == null
+                      ? 'Select End Time'
+                      : 'End Time: ${_endTime!.format(context)}'),
+                ),
+              ),
+            ],
+          ),
+          DropdownButtonFormField<String>(
+            value: _selectedTicketClass,
+            decoration: InputDecoration(labelText: 'Select Ticket Class'),
+            items: [
+              DropdownMenuItem(
+                  value: 'First Class', child: Text('First Class')),
+              DropdownMenuItem(
+                  value: 'Second Class', child: Text('Second Class')),
+              // Add more ticket classes as needed
+            ],
+            onChanged: (value) {
+              setState(() {
+                _selectedTicketClass = value;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final selectedDate = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime.now(),
+                lastDate: DateTime.now().add(Duration(days: 365)),
+              );
+              if (selectedDate != null) {
+                setState(() {
+                  _selectedDate = selectedDate;
+                });
+              }
+            },
+            child: Text(_selectedDate == null
+                ? 'Select Date'
+                : 'Selected Date: ${_selectedDate!.toString().split(' ')[0]}'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                // Perform the search action using the form fields
+                // _startStation, _endStation, _startTime, _endTime,
+                // _selectedTicketClass, _selectedDate
+              }
+            },
+            child: Text('Search Train'),
           ),
         ],
       ),
     );
+  }
+}
+
+String getGreeting() {
+  var hour = DateTime.now().hour;
+
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Night";
   }
 }
