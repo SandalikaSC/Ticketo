3ee84766853c70ff48e14d6762a942bfe6d7e7e0
nadee-darshika <88460737+nadee-darshika@users.noreply.github.com>
2023-08-10 16:50:31 +0530

    solve the problems

--- web_frontend/src/components/controlCenter/CCSidebar.jsx
+++ web_frontend/src/components/controlCenter/CCSidebar.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import {
   FaBars,
@@ -12,64 +11,77 @@ import {
 } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import Logo from "../common/logoText.png";
- 
-const Sidebar = ({children}) => {
-    const[isOpen ,setIsOpen] = useState(false);
-    const toggle = () => setIsOpen (!isOpen);
-    const menuItem=[
-        {
-            path:"/cc/delays",
-            name:"Delays",
-            icon:<FaBell/>
-        },
-        {
-            path:"/cc/tracktrains",
-            name:"Track trains",
-            icon:<FaMapMarkedAlt/>
-        },
-        {
-            path:"/cc/traininfo",
-            name:"Train info",
-            icon:<FaTrain/>
-        },
-        {
-            path:"/cc/assignGuards",
-            name:"Assign Guards",
-            icon:<FaPersonBooth/>
-        },
-        {
-            path:"/cc/stations",
-            name:"Stations",
-            icon:<FaLocationArrow/>
-        }
-    ]
-    return (
-        <div className="container">
-           <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
-               <div className="top_section">
-                   <img style={{display: isOpen ? "block" : "none"}} src={Logo} width={125} alt="logo" className="logo"/>
-                   <div style={{marginLeft: isOpen ? "25px" : "0px", marginTop: "5px"}} className="bars">
-                       <FaBars onClick={toggle}/>
-                   </div>
-               </div>
-               {
-                   menuItem.map((item, index)=>(
-                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                           <div className="icon">{item.icon}</div>
-                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
-                       </NavLink>
-                   ))
-               }
 
-                <div style={{ marginTop: "100px", marginBottom: "20px", textAlign: "center" }}>
-                    <button className="signout" style={{ display: "flex", alignItems: "center" }}>
-                        <div className="icon" style={{marginLeft: "14px"}}>{<FaSignOutAlt/>}</div>
-                        <div style={{display: isOpen ? "block" : "none", marginLeft: "10px"}} className="link_text">Sign Out</div>
-                    </button>
-                </div>
-           </div>
- 
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      path: "/cc/delays",
+      name: "Delays",
+      icon: <FaBell />,
+    },
+    {
+      path: "/cc/tracktrains",
+      name: "Track trains",
+      icon: <FaMapMarkedAlt />,
+    },
+    {
+      path: "/cc/traininfo",
+      name: "Train info",
+      icon: <FaTrain />,
+    },
+    {
+      path: "/cc/assignGuards",
+      name: "Assign Guards",
+      icon: <FaPersonBooth />,
+    },
+    {
+      path: "/cc/stations",
+      name: "Stations",
+      icon: <FaLocationArrow />,
+    },
+  ];
 
+  return (
+    <div className="container">
+      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+        <div className="top_section">
+          <img
+            style={{ display: isOpen ? "block" : "none" }}
+            src={Logo}
+            width={125}
+            alt="logo"
+            className="logo"
+          />
+          <div
+            style={{
+              marginLeft: isOpen ? "25px" : "0px",
+              marginTop: "5px",
+            }}
+            className="bars"
+          >
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeClassName="active"
+          >
+            <div className="icon">{item.icon}</div>
+            <div
+              style={{
+                display: isOpen ? "block" : "none",
+              }}
+              className="link_text"
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
         <div
           style={{
             marginTop: "100px",
@@ -85,7 +97,10 @@ const Sidebar = ({children}) => {
               {<FaSignOutAlt />}
             </div>
             <div
-              style={{ display: isOpen ? "block" : "none", marginLeft: "10px" }}
+              style={{
+                display: isOpen ? "block" : "none",
+                marginLeft: "10px",
+              }}
               className="link_text"
             >
               Sign Out
@@ -94,7 +109,7 @@ const Sidebar = ({children}) => {
         </div>
       </div>
 
-      <main class="cc-main">{children}</main>
+      <main className="cc-main">{children}</main>
     </div>
   );
 };
