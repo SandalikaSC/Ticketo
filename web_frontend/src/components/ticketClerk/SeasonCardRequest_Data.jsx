a9b5f413f33a4a8d7b378cf06e4bbf2e97cf2547
Subodhinie99 <subodhiniechathurajika@gmail.com>
2023-08-12 19:55:32 +0530

    Season_ID_Issueing_details _cards

---
+++ web_frontend/src/components/ticketClerk/SeasonCardRequest_Data.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import '../../css/ticketClerk_applicationForm.css';
+
+const ApplicationData = ({ data }) => {
+    return (
+        <Card className="tc-application-form">
+            <Card.Body className='tc-app-card-body'>
+                <h3 className="tc-form-heading">Season ID Card Request Information</h3>
+                <div className="tc-data-field">
+                    <strong>Full Name:</strong> {data.name}
+                </div>
+            
+                <div className="tc-data-field">
+                    <strong>Telephone Number:</strong> {data.phoneNumber}
+                </div>
+                <div className="tc-data-field">
+                    <strong>Email:</strong> {data.email}
+                </div>
+                <div className="tc-data-field">
+                    <strong>Designation:</strong> {data.designation}
+                </div>
+                <div className="tc-data-field">
+                    <strong>Permanent Address:</strong> {data.permanent_address}
+                </div>
+              
+                <div className="tc-data-field">
+                    <strong>National Identity Card No:</strong> {data.nic}
+                </div>
+                <div className="tc-data-field">
+                    <strong>Institution/Work Place:</strong> {data.work_place}
+                </div>
+                <div className="tc-data-field">
+                    <strong>Institution/Work Place Address:</strong> {data.work_place_address}
+                </div>
+                <div className="tc-data-field">
+                    <strong>Photograph:</strong> {data.id_photograph}
+                </div>
+                <div className="tc-data-field">
+                    <strong>Application Form Photograph:</strong> {data.application_photograph}
+                </div>
+            </Card.Body>
+           <Card.Footer  className="tc-app-card-footer">
+                <Button variant="danger" className="tc-form-approve-button">
+                    Approve
+                </Button>
+                <Button variant="outline-danger" className="tc-form-reject-button">
+                    Reject
+                </Button>
+            </Card.Footer>        
+       
+        </Card>
+    );
+};
+
+export default ApplicationData;
