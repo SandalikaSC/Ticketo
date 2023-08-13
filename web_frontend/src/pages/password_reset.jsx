71ac9fd205561401840e49af4a6682699cb0257d
nadee-darshika <88460737+nadee-darshika@users.noreply.github.com>
2023-08-13 23:57:44 +0530

    password reset

--- web_frontend/src/pages/password_reset.jsx
+++ web_frontend/src/pages/password_reset.jsx
@@ -1,25 +1,157 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logoImage from "../assets/logo.png";
-import { Paper, TextField, Button, Typography, Divider } from "@mui/material";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import {
+  Paper,
+  TextField,
+  Button,
+  Typography,
+  Divider,
+  CircularProgress,
+  IconButton,
+} from "@mui/material";
+
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 
 const PasswordReset = () => {
+  const history = useHistory();
   const [showEmailSection, setShowEmailSection] = useState(true);
   const [showOTPSection, setShowOTPSection] = useState(false);
   const [showPasswordReset, setShowPasswordReset] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [countdown, setCountdown] = useState(120);
+  const [otpDigits, setOtpDigits] = useState(["", "", "", ""]);
+  const [formData, setFormData] = useState({
+    email: "",
+    mobileNumber: "", // Initialize other formData fields here
+  });
+  // const [showPasswordReset, setShowPasswordReset] = useState(true);
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  const handleSendOTP = () => {
-    setShowEmailSection(false);
-    setShowOTPSection(true);
+  useEffect(() => {
+    let timer;
+    if (showOTPSection) {
+      // Display the countdown timer after 1 second
+      timer = setTimeout(() => {
+        startCountdown();
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [showOTPSection]);
+
+  const startCountdown = () => {
+    const timer = setInterval(() => {
+      if (countdown > 0) {
+        setCountdown(countdown - 1);
+      } else {
+        clearInterval(timer);
+      }
+    }, 1000); // Update every second
   };
 
-  const handleVerifyOTP = () => {
-    setShowOTPSection(false);
-    setShowPasswordReset(true);
+  const handleSendOTP = async (formData) => {
+    setLoading(true);
+    try {
+      console.log(formData.email);
+      const response = await axios.post(
+        "http://localhost:5000/api/generate-otp",
+        { email: formData.email },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        setShowEmailSection(false);
+        setShowOTPSection(true);
+      } else {
+        // Handle error here
+        console.error("Failed to send OTP");
+      }
+    } catch (error) {
+      console.error("Error sending OTP:", error);
+    } finally {
+      setLoading(false); // Set loading back to false
+    }
   };
 
-  const handlePasswordReset = () => {
-    // Add logic here to reset the password
+  const handleVerifyOTP = async () => {
+    setLoading(true);
+    const otpValue = parseInt(otpDigits.join(""), 10);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/verify-otp",
+        {
+          email: formData.email,
+          mobileNumber: formData.mobileNumber,
+          otp: otpValue,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("hiii");
+      if (response.status === 200) {
+        setShowOTPSection(false);
+        setShowPasswordReset(true);
+      } else {
+        console.error("OTP verification failed");
+      }
+    } catch (error) {
+      console.error("Error verifying OTP:", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const handlePasswordReset = async () => {
+    if (newPassword === confirmPassword) {
+      // Passwords match, proceed with reset
+      const dataToSend = {
+        email: formData.email, // Replace with the actual email
+        mobileNumber: "", // Replace with the actual mobile number
+        password: newPassword,
+        confirmPassword: confirmPassword,
+      };
+
+      try {
+        const response = await axios.post(
+          "http://localhost/api/reset-password",
+          dataToSend
+        );
+
+        if (response.status === 200) {
+          // Password reset successful, redirect to login page
+          history.push("/login"); // Use the history object to navigate to the login page
+        } else {
+          console.log("Password reset failed");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      // Passwords don't match
+      setPasswordsMatch(false);
+    }
+  };
+  if (showOTPSection) {
+    startCountdown();
+    // Rest of your JSX code...
+  }
 
   return (
     <div
@@ -80,6 +212,11 @@ const PasswordReset = () => {
               <TextField
                 type="email"
                 placeholder="Email address"
+                value={formData.email} // Use formData.email here
+                onChange={
+                  (event) =>
+                    setFormData({ ...formData, email: event.target.value }) // Update email in formData
+                }
                 fullWidth
                 style={{ marginBottom: "10px", borderRadius: "25px" }}
               />
@@ -95,15 +232,16 @@ const PasswordReset = () => {
                   display: "block",
                   margin: "0 auto",
                 }}
-                onClick={handleSendOTP}
+                onClick={() => handleSendOTP(formData)}
+                disabled={loading}
               >
-                Send OTP
+                {loading ? <CircularProgress size={24} /> : "Send OTP"}
               </Button>
             </div>
           </div>
         )}
 
-        {showOTPSection && (
+        {/* {showOTPSection && (
           <div>
             <img
               src={logoImage}
@@ -149,7 +287,13 @@ const PasswordReset = () => {
             >
               <input
                 type="text"
-                maxLength={4}
+                maxLength={1}
+                value={digit}
+                onChange={(event) => {
+                  const newDigits = [...otpDigits];
+                  newDigits[index] = event.target.value;
+                  setOtpDigits(newDigits);
+                }}
                 style={{
                   width: "60px",
                   textAlign: "center",
@@ -162,7 +306,9 @@ const PasswordReset = () => {
               <span style={{ fontSize: "1.5em" }}> </span>
               <input
                 type="text"
-                maxLength={4}
+                maxLength={1}
+                value={otp}
+                onChange={(event) => setOTP(event.target.value)}
                 style={{
                   width: "60px",
                   textAlign: "center",
@@ -175,7 +321,9 @@ const PasswordReset = () => {
               <span style={{ fontSize: "1.5em" }}> </span>
               <input
                 type="text"
-                maxLength={4}
+                maxLength={1}
+                value={otp}
+                onChange={(event) => setOTP(event.target.value)}
                 style={{
                   width: "60px",
                   textAlign: "center",
@@ -188,7 +336,9 @@ const PasswordReset = () => {
               <span style={{ fontSize: "1.5em" }}> </span>
               <input
                 type="text"
-                maxLength={4}
+                maxLength={1}
+                value={otp}
+                onChange={(event) => setOTP(event.target.value)}
                 style={{
                   width: "60px",
                   textAlign: "center",
@@ -214,6 +364,85 @@ const PasswordReset = () => {
             >
               Verify OTP
             </Button>
+            {countdown > 0 && (
+              <Typography
+                variant="body1"
+                style={{ fontWeight: "600", marginBottom: "5px" }}
+              >
+                Time Left: {Math.floor(countdown / 60)}:
+                {(countdown % 60).toString().padStart(2, "0")}
+              </Typography>
+            )}
+          </div>
+        )} */}
+
+        {showOTPSection && (
+          <div>
+            <img
+              src={logoImage}
+              alt="Logo"
+              style={{ width: "20%", margin: "0 auto" }}
+            />
+            <Typography variant="subtitle1">
+              Where your train adventure begins
+            </Typography>
+            <Divider />
+            <Typography variant="body1">
+              Enter your received OTP below.
+            </Typography>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {otpDigits.map((digit, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  maxLength={1}
+                  value={digit}
+                  onChange={(event) => {
+                    const newDigits = [...otpDigits];
+                    newDigits[index] = event.target.value;
+                    setOtpDigits(newDigits);
+                  }}
+                  style={{
+                    width: "60px",
+                    textAlign: "center",
+                    fontSize: "1.5em",
+                    marginRight: "10px",
+                    borderRadius: "8px",
+                    border: "1px solid #ccc",
+                  }}
+                />
+              ))}
+            </div>
+            <Button
+              variant="contained"
+              color="primary"
+              fullWidth
+              style={{
+                backgroundColor: "#FA6F5D",
+                borderRadius: "25px",
+                fontSize: "1em",
+                margin: "20px auto",
+              }}
+              onClick={handleVerifyOTP}
+              disabled={loading}
+            >
+              {loading ? <CircularProgress size={24} /> : "Verify OTP"}
+            </Button>
+            {/* {countdown > 0 && (
+              <Typography
+                variant="body1"
+                style={{ fontWeight: "600", marginBottom: "5px" }}
+              >
+                Time Left: {Math.floor(countdown / 60)}:
+                {(countdown % 60).toString().padStart(2, "0")}
+              </Typography>
+            )} */}
           </div>
         )}
 
@@ -250,17 +479,36 @@ const PasswordReset = () => {
                 Enter new password:
               </Typography>
               <TextField
-                type="password"
+                type={showPassword ? "text" : "password"}
                 placeholder="New Password"
                 fullWidth
                 style={{ marginBottom: "10px", borderRadius: "25px" }}
+                value={newPassword}
+                onChange={(event) => setNewPassword(event.target.value)}
               />
               <TextField
-                type="password"
+                type={showConfirmPassword ? "text" : "password"}
                 placeholder="Confirm New Password"
                 fullWidth
                 style={{ marginBottom: "10px", borderRadius: "25px" }}
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassword(event.target.value)}
               />
+              {!passwordsMatch && (
+                <Typography variant="caption" color="error">
+                  Passwords do not match.
+                </Typography>
+              )}
+              <div>
+                <IconButton onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+                <IconButton
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  {showConfirmPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </div>
               <Button
                 variant="contained"
                 color="primary"
