6a0a59ce768d237f8122e5eb08261b7798309938
SandalikaSC <66319075+SandalikaSC@users.noreply.github.com>
2023-09-26 20:17:52 +0530

    load schedules for ticket

--- passenger_frontend/lib/screens/trainSchedules.dart
+++ passenger_frontend/lib/screens/trainSchedules.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:passenger_frontend/constants/app_styles.dart';
 import 'package:passenger_frontend/modals/ReservationTicket.dart';
+import 'package:passenger_frontend/services/trainScheduleService.dart';
+import 'package:passenger_frontend/utils/error_handler.dart';
 import 'package:passenger_frontend/widgets/ScheduleWidject.dart';
+
+import '../modals/schedule.dart';
 
 class TrainSchedule extends StatefulWidget {
 
@@ -14,10 +20,44 @@ class TrainSchedule extends StatefulWidget {
 }
 
 class _TrainScheduleState extends State<TrainSchedule> {
+  List<Schedule> _schedules = [];
 
   void _loadSchedules() async {
     if (!mounted) return;
-    final response = await trainScheduleService.loadSchedules();
+try {
+  final TrainScheduleService trainScheduleService = TrainScheduleService();
+  final response = await trainScheduleService.getSchedules(
+      widget.reservationTicket);
+  final responseData = json.decode(response.body);
+  // Print the response body (content)
+
+  if (response.statusCode == 200) {
+    final dynamic decodedResponse = json.decode(response.body);
+
+    if (decodedResponse != null && decodedResponse['stations'] != null) {
+      final List<dynamic> data = decodedResponse['stations'];
+      setState(() {
+        _schedules = data
+            .map((scheduleData) =>
+            Schedule(
+                startStation: scheduleData['startStation'] ?? "",
+                endStation: scheduleData['endStation'] ?? '',
+                startTime: scheduleData['startTime'] ?? "",
+                endTime: scheduleData['endTime'] ?? ""
+            ))
+            .toList();
+      });
+    } else {
+      _schedules = List.empty();
+    }
+  }
+}catch(e){
+  print('Error occurred: $e'); // Print the exception details
+  ErrorHandler.showErrorSnackBar(
+      context, 'Unknown error occurred. Please try again later.');
+}
+
+
   }
 
   @override
@@ -67,51 +107,70 @@ class _TrainScheduleState extends State<TrainSchedule> {
       ),
       body: ListView.builder(
         // Only one scheduleWidget for now
+        itemCount: _schedules.length,
         itemBuilder: (BuildContext context, int index) {
+          final schedule = _schedules[index]; // Get the schedule at the current index
+
           return Column(
             children: [
               scheduleWidget(
-                classname: "First class",
-                end: "Colombo Fort",
-                start: "Galle",
-                StartTime: "08:55 AM",
-                name: "Gale Kumari",
-                passengers: 5,
-                endTime: "10:15 AM",
-                date: "2023-08-15",
+                classname: widget.reservationTicket.classname,
+                end: schedule.endStation,
+                start: schedule.startStation,
+                StartTime: schedule.startTime,
+                name: schedule.trainName ?? "",
+                endTime: schedule.endTime ??"",
+                date: schedule.date ?? "",
               ),
-              scheduleWidget(
-                classname: "First class",
-                end: "Colombo Fort",
-                start: "Matara",
-                StartTime: "06:55 AM",
-                name: "Ruhunu Kumari",
-                passengers: 5,
-                endTime: "09:25 AM",
-                date: "2023-08-15",
-              ),
-              scheduleWidget(
-                classname: "First class",
-                end: "Beliatta",
-                start: "Maradana",
-                StartTime: "02:55 PM",
-                name: "Gale Kumari",
-                passengers: 5,
-                endTime: "06:30 PM",
-                date: "2023-08-16",
-              ),
-              scheduleWidget(
-                classname: "First class",
-                end: "Galle",
-                start: "Maradana",
-                StartTime: "04:55 AM",
-                name: "Sagarika",
-                passengers: 5,
-                endTime: "08:20 AM",
-                date: "2023-08-16",
-              ),
+              // Add any desired spacing or dividers between scheduleWidget items.
+              SizedBox(height: 16.0), // Example: Adds 16 pixels of vertical space
+              Divider(), // Example: Adds a horizontal divider line
             ],
           );
+          // return Column(
+          //   children: [
+          //     scheduleWidget(
+          //       classname: "First class",
+          //       end: "Colombo Fort",
+          //       start: "Galle",
+          //       StartTime: "08:55 AM",
+          //       name: "Gale Kumari",
+          //       passengers: 5,
+          //       endTime: "10:15 AM",
+          //       date: "2023-08-15",
+          //     ),
+          //     scheduleWidget(
+          //       classname: "First class",
+          //       end: "Colombo Fort",
+          //       start: "Matara",
+          //       StartTime: "06:55 AM",
+          //       name: "Ruhunu Kumari",
+          //       passengers: 5,
+          //       endTime: "09:25 AM",
+          //       date: "2023-08-15",
+          //     ),
+          //     scheduleWidget(
+          //       classname: "First class",
+          //       end: "Beliatta",
+          //       start: "Maradana",
+          //       StartTime: "02:55 PM",
+          //       name: "Gale Kumari",
+          //       passengers: 5,
+          //       endTime: "06:30 PM",
+          //       date: "2023-08-16",
+          //     ),
+          //     scheduleWidget(
+          //       classname: "First class",
+          //       end: "Galle",
+          //       start: "Maradana",
+          //       StartTime: "04:55 AM",
+          //       name: "Sagarika",
+          //       passengers: 5,
+          //       endTime: "08:20 AM",
+          //       date: "2023-08-16",
+          //     ),
+          //   ],
+          // );
         },
       ),
     );
