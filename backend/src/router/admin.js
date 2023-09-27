c117172412888e0f0a250466bf83a26c4b294c8e
ParindiDewmini <paridew99@gmail.com>
2023-09-27 12:27:34 +0530

    report view change

--- backend/src/router/admin.js
+++ backend/src/router/admin.js
@@ -1,7 +1,9 @@
+// backend/src/router/admin.js
 const express = require('express');
 const reportsController = require('../controllers/admin/reportsController');
 const router = express.Router();
 
+// Define the '/income' route in the admin router
 router.get('/income', reportsController.generateIncomeReport);
 
 module.exports = router;
--- web_frontend/src/components/admin/IncomeReportView.jsx
+++ web_frontend/src/components/admin/IncomeReportView.jsx
@@ -1,66 +1,34 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Document, Page, Text, View, StyleSheet, PDFViewer } from '@react-pdf/renderer';
-
-const dummyTableData = [
-  { date: '2023.07.01', amount: '1,000,000', station: 'Colombo Fort' },
-  { date: '2023.07.01', amount: '2,000,000', station: 'Maradana' },
-  { date: '2023.07.01', amount: '1,050,000', station: 'Kandy' },
-  { date: '2023.07.02', amount: '200,000', station: 'Hikkaduwa' },
-  { date: '2023.07.02', amount: '600,000', station: 'Galle' },
-  { date: '2023.07.02', amount: '400,000', station: 'Boossa' },
-  { date: '2023.07.03', amount: '100,000', station: 'Nanu Oya' },
-  { date: '2023.07.03', amount: '200,000', station: 'Peradeniya' },
-  { date: '2023.07.03', amount: '700,000', station: 'Bentota' },
-  { date: '2023.07.04', amount: '500,000', station: 'Kaluthara ' },
-  { date: '2023.07.04', amount: '100,000', station: 'Dehiwala' },
-  { date: '2023.07.04', amount: '300,000', station: 'Moratuwa' },
-  { date: '2023.07.04', amount: '700,000', station: 'Thalpe' },
-  { date: '2023.07.04', amount: '500,000', station: 'Unawatuna ' },
-  { date: '2023.07.04', amount: '100,000', station: 'Midigama' },
-  { date: '2023.07.04', amount: '300,000', station: 'Habaraduwa' },
-  { date: '2023.07.04', amount: '1,000,000', station: 'Unawatuna' },
-  // Add more data here...
-];
+import axios from 'axios'; // Import Axios for making API requests
 
 const styles = StyleSheet.create({
-  reportContainer: {
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  table: {
-    border: '1pt solid black', // Adding border to the entire table
-    width: '100%',
-  },
-  tableHeader: {
-    flexDirection: 'row',
-    borderBottom: 1,
-    alignItems: 'center',
-    paddingBottom: 5,
-    border: '1pt solid black', // Adding border to the table header cells
-  },
-  tableHeaderCell: {
-    width: '25%',
-    fontWeight: 'bold',
-  },
-  tableRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingTop: 5,
-    border: '1pt solid black', // Adding border to the table row cells
-  },
-  tableCell: {
-    width: '25%',
-  },
+  // Define your styles here
 });
 
 const IncomeReportView = ({ reportData }) => {
   const pdfRef = useRef();
+  const [reportTableData, setReportTableData] = useState([]); // State to store report data
 
-  // Extract data from reportData
-  const { selectedOption, startDate, endDate, selectedMonth, selectedYear } = reportData;
+  useEffect(() => {
+    // Function to fetch report data from the backend
+    const fetchReportData = async () => {
+      try {
+        // Make an API request to your backend here, sending the reportData as query parameters
+        const response = await axios.get('/api/reports/income', {
+          params: reportData,
+        });
+
+        // Update the reportTableData state with the data received from the backend
+        setReportTableData(response.data);
+      } catch (error) {
+        console.error('Error fetching report data:', error);
+      }
+    };
+
+    // Call the fetchReportData function when the component mounts
+    fetchReportData();
+  }, [reportData]);
 
   // Create PDF report content using react-pdf
   const pdfReport = (
@@ -73,11 +41,11 @@ const IncomeReportView = ({ reportData }) => {
             <Text style={styles.tableHeaderCell}>Amount(LKR)</Text>
             <Text style={styles.tableHeaderCell}>Station</Text>
           </View>
-          {dummyTableData.map((row, index) => (
+          {reportTableData.map((row, index) => (
             <View key={index} style={styles.tableRow}>
               <Text style={styles.tableCell}>{row.date}</Text>
-              <Text style={styles.tableCell}>{row.amount}</Text>
-              <Text style={styles.tableCell}>{row.station}</Text>
+              <Text style={styles.tableCell}>{row.total_amount}</Text>
+              {/* Add more fields here */}
             </View>
           ))}
         </View>
