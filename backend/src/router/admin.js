3f599bfe2e668948f9da07f955293baa71663eee
ParindiDewmini <paridew99@gmail.com>
2023-10-04 16:38:52 +0530

    income-report-view

--- backend/src/router/admin.js
+++ backend/src/router/admin.js
@@ -1,35 +1,9 @@
+// admin.js
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const { PrismaClient } = require('@prisma/client');
-const authRouter = require('./router/authRouter');
-const stationMasterRoutes = require('./router/stationMasterRoutes');
-const adminRouter = require('./router/admin'); // Import the admin router
+const reportsController = require('/../controllers/admin/reportsController');
+const router = express.Router();
 
-const cors = require('cors');
+// Define the '/income' route in the admin router
+router.get('/income', reportsController.generateIncomeReport);
 
-const prisma = new PrismaClient();
-const app = express();
-const port = process.env.PORT || 5000;
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-  origin: ['http://localhost:3000', '*'],
-  credentials: true, // Allow cookies to be sent along with the request
-}));
-
-// Routes
-app.use("/api", authRouter);
-app.use('/api', stationMasterRoutes);
-
-// Use the admin router for /api/reports/income
-app.use('/api/reports', adminRouter);
-
-// Start the server
-app.listen(port, () =>
-{
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = router;
