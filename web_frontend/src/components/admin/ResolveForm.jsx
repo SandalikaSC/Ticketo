2d17f134a72a75f5760a392e2355a3a0ab7cf1d3
ParindiDewmini <paridew99@gmail.com>
2023-11-01 17:42:14 +0530

    Update ResolveForm.jsx

--- web_frontend/src/components/admin/ResolveForm.jsx
+++ web_frontend/src/components/admin/ResolveForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Swal from 'sweetalert2'; // Import SweetAlert
+import Swal from 'sweetalert2';
 
 const ResolveForm = ({ complaintData, onResolve }) => {
   const [comment, setComment] = useState('');
@@ -15,11 +15,13 @@ const ResolveForm = ({ complaintData, onResolve }) => {
       title: 'Complaint Resolved',
       text: 'The complaint has been resolved successfully.',
       icon: 'success',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // You can add any specific action you want to perform here.
+        // For example, clear the form fields or close the SweetAlert.
+        // This code will keep the user on the current page.
+      }
     });
-
-    // Reset the form fields
-    setComment('');
-    setMessage('');
   };
 
   return (
